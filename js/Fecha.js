02fede4eaa53d29eee32587bdbfd324c58021d6b
Cristy <obichamte.life@gmail.com>
2020-06-14 23:08:31 +0200

    no he sabido cambiar el tamanyo de la imagen

---
+++ js/Fecha.js
@@ -0,0 +1,15 @@
+//Fecha
+
+var dia = new Date();
+
+//    document.write(`día ${dia.getDate} <br> Dia de la semana: ${dia.getDay}`);
+
+
+document.write(`Dia: ${dia.getDate()}, <br> Dia de la semana(0 al 6: ${dia.getDay()}`);
+
+switch (dia.getday = 0) {
+    case dia.getday = 0:
+        
+    
+    }
+    }
---
+++ js/imagengrande.js
@@ -0,0 +1,56 @@
+
+
+
+
+var imagen = document.getElementById('imagen');
+
+
+
+
+
+img.file = this.files[0];
+const imagen = new imagen();
+imagen.onload = (function (img) {
+    return function () {
+        img.src
+
+
+function incrementar(){ 
+
+    img.style.width = `${img.width + = 100}px`;
+    img.style.height = `${img.height += 50}px`;
+    
+    function reducir() {
+        if (img.width <= 100 || img.height <= 50) return;
+
+        img.style.width = `${img.width -= 100}px`;
+        img.style.height = `${img.height -= 50}px`;
+
+
+
+
+
+
+
+    
+
+    
+
+    document.body.appendChild(imagen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
